27b757f00bc22042db12448eb71adca1991d24ac
Your Name <you@example.com>
2016-05-25 11:57:05 +0200

    Zadanie 3

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -6,25 +6,31 @@ describe('app', function ()
 
     describe('returnOnlyLetter', function () {
         it('should return only letter', function () {
-            expect(answers.returnOnlyLetter('a4l1f5a')).toBe('al4fa');
+            expect(answers.returnOnlyLetter('a4l1f5a')).toBe('alfa');
+            expect(answers.returnOnlyLetter('w9i2s0l2a')).toBe('wisla');
         });
         it('should return empty string', function () {
-            expect(answers.returnOnlyLetter('45345')).toBe('4');
+            expect(answers.returnOnlyLetter('45345')).toBe('');
+            expect(answers.returnOnlyLetter('!@#5431')).toBe('');
         });
         it('number should return false', function () {
-            expect(answers.returnOnlyLetter(45345)).toEqual(true);
+            expect(answers.returnOnlyLetter(45345)).toEqual(false);
+            expect(answers.returnOnlyLetter(1906)).toEqual(false);
         });
     });
 
     describe('alphabetOrder', function () {
         it('should return letters in alphabetical order', function () {
             expect(answers.alphabetOrder('alfa')).toBe('aafl');
+            expect(answers.alphabetOrder('zbyszek')).toBe('beksyzz');
         });
         it('should return lower case letters in alphabetical order', function () {
             expect(answers.alphabetOrder('AlfA')).toBe('aafl');
+            expect(answers.alphabetOrder('ZbySzek')).toBe('beksyzz');
         });
         it('should return only letters in alphabetical order', function () {
-            expect(answers.alphabetOrder('A1l2f3a')).toBe('a1afl');
+            expect(answers.alphabetOrder('A1l2f3a')).toBe('aafl');
+            expect(answers.alphabetOrder('Z1b2y3s4z5e6k7')).toBe('beksyzz');
         });
      });
 });
